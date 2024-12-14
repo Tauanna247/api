416ff5dce0b247c23c384aee8a0d1abc8cd786fe
Tauanna247 <tlns@aluno.ifnmg.edu.br>
2024-12-14 01:43:33 +0000

    atividade

---  index.js
+++  index.js
@@ -1,15 +1,33 @@
-import express from "express";      
-const app = express();             
-const port = 3000;                  
+//index.js
+import dotenv from "dotenv";
+import express from "express";
+import { selectUsuarios } from "./bd.js";
 
-app.get("/", (req, res) => {       
-  console.log("Rota GET/ solicitada");
+dotenv.config();
+
+const app = express(); // Instancia o Express
+const port = 3000; // Define a porta
+
+app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
+  // Cria a rota da raiz do projeto
   res.json({
-    nome: "Tauanna Larissa Neres Dos Santos",     
+    nome: "Tauanna Larissa Neres Dos Santos",
   });
 });
 
-app.listen(port, () => {            
-  console.log(`Serviço escutando na porta:  ${port}`);
+app.get("/usuario", async (req, res) => {
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+
+  console.log("Rota GET/usuario solicitada");
 });
 
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # api
+Tauanna Larissa Neres Dos Santos
